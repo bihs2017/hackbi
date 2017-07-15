e872a77addb1e25c55f3a0dd6384b0651601c278
BCHoagland <bhoggland@gmail.com>
2017-07-15 16:34:48 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function updateHideShow() {
 	$('.hideme').each( function(i) {
-		var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+		var bottom_of_object = $(this).offset().top + ($(this).outerHeight() / 2);
 		var bottom_of_window = $(window).scrollTop() + $(window).height();
 		if ( bottom_of_window > bottom_of_object ) {
 			$(this).addClass('showme');
